3efe71887e950d16f41f3384f442997bd16f49dc
Dominik <d.jasinski@ideosoftware.com>
2026-05-19 22:52:20 +0200

    fix: rector coding standards

--- server/app/Actions/Fortify/CreateNewUser.php
+++ server/app/Actions/Fortify/CreateNewUser.php
@@ -10,8 +10,8 @@ use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules, ProfileValidationRules;
-
+    use PasswordValidationRules;
+    use ProfileValidationRules;
     /**
      * Validate and create a newly registered user.
      *
@@ -24,7 +24,7 @@ class CreateNewUser implements CreatesNewUsers
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
+        return User::query()->create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => $input['password'],
